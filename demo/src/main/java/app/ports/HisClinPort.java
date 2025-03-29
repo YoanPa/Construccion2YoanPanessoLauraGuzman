ac05718208a53b58d5df6b1804d77b0b1051020e
LauraVGuzman <laura.guzman77@correo.tdea.edu.co>
2025-03-29 02:24:23 -0500

    init

--- demo/src/main/java/app/ports/HisClinPort.java
+++ demo/src/main/java/app/ports/HisClinPort.java
@@ -1,5 +1,11 @@
 package app.ports;
 
-public class HisClinPort {
-
+import app.domain.models.HisClin;
+//talves aca toque poner la creacion y anulacion de order
+public interface HisClinPort {
+	public void saveHisClin(HisClin hisclin);
+	public HisClin FindbyHisClin(HisClin hisclin);
 }
+/*
+ * 
+ */
--- demo/src/main/java/app/ports/OrderPort.java
+++ demo/src/main/java/app/ports/OrderPort.java
@@ -1,5 +1,10 @@
 package app.ports;
 
-public interface OrderPort {
+import app.domain.models.Order;
 
+public interface OrderPort {
+	public Order createOrder(Order order);
+	public Order deleteOrder(Order order);
+	public Order findbyOrderId(Order orderId);
+	
 }

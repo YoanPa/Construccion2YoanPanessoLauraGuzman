845369192ebe4af124531789a7a4fab21a256ef2
LauraVGuzman <laura.guzman77@correo.tdea.edu.co>
2025-03-31 00:59:02 -0500

    init
    init

--- demo/src/main/java/app/adapters/person/PersonAdapter.java
+++ demo/src/main/java/app/adapters/person/PersonAdapter.java
@@ -2,6 +2,7 @@ package app.adapters.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+<<<<<<< HEAD
 import app.adapters.person.entity.PersonEntity;
 import app.adapters.person.repository.PersonRepository;
 import app.domain.models.Person;
@@ -21,17 +22,75 @@ public class PersonAdapter implements PersonPort {
 	@Override
 	public boolean existPerson(long personId) {
 		return personRepository.existsByPersonId(personId);
+=======
+
+import app.adapters.person.entity.PersonEntity;
+import app.adapters.person.repository.PersonRepository;
+import app.domain.models.Person;
+import app.ports.PersonPort;
+
+@Service
+public class PersonAdapter implements PersonPort {
+    
+    @Autowired
+    private PersonRepository personRepository;
+    
+    @Override
+    public Person findById(Long id) {
+        PersonEntity personEntity = personRepository.findById(id);
+        if (personEntity == null) {
+            return null;
+        }
+        return personAdapter(personEntity);
+    }
+    
+    @Override
+    public void save(Person person) {
+        PersonEntity personEntity = personAdapter(person);
+        personRepository.save(personEntity);
+        person.setPersonId(personEntity.getPersonId());
+    }
+    
+    private PersonEntity personAdapter(Person person) {
+        PersonEntity personEntity = new PersonEntity();
+        personEntity.setPersonId(person.getPersonId());
+        personEntity.setName(person.getName());
+        personEntity.setCellPhone(person.getCellPhone());
+        personEntity.setDocument(person.getDocument());
+        return personEntity;
+    }
+    
+    private Person personAdapter(PersonEntity personEntity) {
+        Person person = new Person();
+        person.setPersonId(personEntity.getPersonId());
+        person.setName(personEntity.getName());
+        person.setCellPhone(personEntity.getCellPhone());
+        person.setDocument(personEntity.getDocument());
+        return person;
+    }
+
+	@Override
+	public boolean existPerson(long personId) {
+		// TODO Auto-generated method stub
+		return false;
+>>>>>>> 22717d5177611efb1713069b1789642c21527833
 	}
 
 	@Override
 	public void savePerson(Person person) {
+<<<<<<< HEAD
 		PersonEntity personEntity = personAdapter(person);
 		personRepository.save(personEntity);
 		person.setPersonId(personEntity.getPersonId());
+=======
+		// TODO Auto-generated method stub
+		
+>>>>>>> 22717d5177611efb1713069b1789642c21527833
 	}
 
 	@Override
 	public Person findBypersonId(long personId) {
+<<<<<<< HEAD
 		PersonEntity personEntity = personRepository.findByPersonId(personId);
 		return personAdapter(personEntity);
 	}
@@ -52,5 +111,9 @@ public class PersonAdapter implements PersonPort {
 		personEntity.setAge(person.getAge());
 		personEntity.setRole(person.getRole());
 		return personEntity;
+=======
+		// TODO Auto-generated method stub
+		return null;
+>>>>>>> 22717d5177611efb1713069b1789642c21527833
 	}
 }
